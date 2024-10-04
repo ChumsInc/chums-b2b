53c153e8a1fd407f537578d369b6a2f157a3f219
Steve Montgomery <steve@chums.com>
2024-10-04 11:19:43 -0600

    fix: remove debugging comments fom SSR render.tsx

--- src/server/render.tsx
+++ src/server/render.tsx
@@ -56,23 +56,23 @@ export async function renderApp(req: Request, res: Response, next: NextFunction)
             return;
         }
         const nonce:string = res.locals.cspNonce!;
-        debug('renderApp()', 'loading manifest');
+        // debug('renderApp()', 'loading manifest');
         const manifestFiles = await loadManifest();
-        debug('renderApp()', 'loading preloaded state');
+        // debug('renderApp()', 'loading preloaded state');
         const preload = await loadJSON<PreloadedState>(`http://localhost:${API_PORT}/preload/state.json`);
         if (!preload.version) {
-            debug('renderApp()', 'loading version');
+            // debug('renderApp()', 'loading version');
             const versionNo = await loadVersionNo();
             preload.version = {versionNo}
         }
 
         const initialState = prepState(preload ?? {});
         initialState.app.nonce = nonce;
-        debug('renderApp()', 'configuring Store');
+        // debug('renderApp()', 'configuring Store');
         const store = configureStore({reducer: rootReducer, preloadedState: initialState});
         const helmetData = new HelmetData({});
 
-        debug('renderApp()', 'rendering to string');
+        // debug('renderApp()', 'rendering to string');
         const app = renderToString(
             <Provider store={store}>
                 <HelmetProvider context={helmetData.context}>
@@ -83,16 +83,17 @@ export async function renderApp(req: Request, res: Response, next: NextFunction)
             </Provider>
         );
         let swatchMTime = 0;
-        debug('renderApp()', 'loading swatch css');
+        // debug('renderApp()', 'loading swatch css');
         try {
             const stat = await fs.stat("./public/b2b-swatches/swatches.css");
             swatchMTime = stat.mtimeMs ?? 0;
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (err: unknown) {
             //Do nothing here
         }
 
         const css = await loadMainCSS();
-        debug('renderApp()', 'rendering to final html');
+        // debug('renderApp()', 'rendering to final html');
         const _html = renderToString(<B2BHtml html={app} css={css} state={store.getState()} cspNonce={nonce}
                                               manifestFiles={manifestFiles} helmet={helmetData.context.helmet}
                                               swatchTimestamp={swatchMTime.toString(36)}/>);
