68bc7f9269f888ad77dabd335be6f82be150b379
Steve Montgomery <steve@chums.com>
2024-12-27 11:39:09 -0700

    fix: - cart checkout: require a valid shipping method before setting customer shipping account - cart checkout: set FOB to format "SLC~X" - cart checkout: add shipping method "USPS Ground Advantage", fix shipping code for USPS Priority

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -22,7 +22,8 @@ export const ShippingMethods: ShippingMethodList = {
         carrier: 'ups',
         enabled: true
     },
-    'APP': {code: 'APP', description: 'USPS Priority', allowCustomerAccount: false, carrier: 'usps', enabled: true},
+    'APP_PRI_EXP ': {code: 'APP_PRI_EXP', description: 'USPS Priority', allowCustomerAccount: false, carrier: 'usps', enabled: true},
+    'USPS GND ADV': {code: 'USPS GND ADV', description: 'USPS Ground Advantage', allowCustomerAccount: false, carrier: 'usps', enabled: true},
 
     '1FEX_1ST_ONIGHT': {
         code: '1FEX_1ST_ONIGHT',
