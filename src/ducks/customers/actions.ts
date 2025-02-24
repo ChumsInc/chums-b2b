4585e711932318aad4e5d24433f1b645ebc48a11
Steve Montgomery <steve@chums.com>
2025-02-24 15:02:46 -0700

    test: - prevent checking if token is expired before loading customer list. If the client computer has a bad time, then it could prevent the request even if the token is valid, so let the server handle it.

--- src/ducks/customers/actions.ts
+++ src/ducks/customers/actions.ts
@@ -5,19 +5,17 @@ import {fetchCustomerList} from "@api/customer-list";
 import {RootState} from "@app/configureStore";
 import {selectLoggedIn} from "../user/selectors";
 import {selectCustomersLoading} from "./selectors";
-import {isTokenExpired} from "@utils/jwtHelper";
-import {auth} from "@api/IntranetAuthService";
 import localStore from "../../utils/LocalStore";
 import {STORE_CUSTOMERS_FILTER_REP, STORE_CUSTOMERS_FILTER_STATE, STORE_RECENT_ACCOUNTS} from "@constants/stores";
 
 export const setCustomersFilter = createAction<string>('customers/setFilter');
-export const setCustomersRepFilter = createAction('customers/setRepFilter', (arg:string|null) => {
+export const setCustomersRepFilter = createAction('customers/setRepFilter', (arg: string | null) => {
     localStore.setItem<string>(STORE_CUSTOMERS_FILTER_REP, arg ?? '');
     return {
         payload: arg
     }
 });
-export const setCustomersStateFilter = createAction('customers/setStateFilter', (arg:string|null) => {
+export const setCustomersStateFilter = createAction('customers/setStateFilter', (arg: string | null) => {
     localStore.setItem<string>(STORE_CUSTOMERS_FILTER_STATE, arg ?? '');
     return {
         payload: arg
@@ -30,7 +28,7 @@ export const clearRecentCustomers = createAction('customers/clearRecentCustomers
     return {payload: null}
 });
 
-export const loadCustomerList = createAsyncThunk<Customer[], UserCustomerAccess | null, {state: RootState}>(
+export const loadCustomerList = createAsyncThunk<Customer[], UserCustomerAccess | null, { state: RootState }>(
     'customers/list/load',
     async (arg) => {
         return await fetchCustomerList(arg!)
@@ -38,11 +36,9 @@ export const loadCustomerList = createAsyncThunk<Customer[], UserCustomerAccess 
     {
         condition: (arg, {getState}) => {
             const state = getState();
-            const token = auth.getToken();
             return selectLoggedIn(state)
                 && !!arg?.isRepAccount
-                && selectCustomersLoading(state) === 'idle'
-                && !isTokenExpired(token);
+                && selectCustomersLoading(state) === 'idle';
         }
     }
 )
