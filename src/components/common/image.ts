197b5acc1b7207f34aeb8e47b38354f96011a031
Steve Montgomery <steve@chums.com>
2026-03-03 07:47:53 -0700

    fix: handle wildcard characters in product images and improve color code validation

--- src/components/common/image.ts
+++ src/components/common/image.ts
@@ -18,8 +18,20 @@ export function parseImageFilename(productImage: string, colorCode?: string | nu
         ga4Exception('Invalid product image', false);
         return 'missing-placeholder2.jpg';
     }
+
+    if (!colorCode || !/[*?]/.test(productImage)) {
+        return productImage.replace(/\s/g, '%20');
+    }
+
+    // this method is generally deprecated, but may still exist in some items
+    // replaces * or ? wildcards in the image filename with a single digit or character of the color code string
+    // for example:
+    // image: '12115?.jpg', colorCode: '100' returns '12115100.jpg'
+    // image: '12115**-**.jpg', colorCode: 'abcd' returns '12115ab-cd.jpg'
+
     let image: string = productImage.replace(/\?/, colorCode ?? '');
-    if (colorCode) {
+
+    if (colorCode && /[A-Z0-9]/i.test(colorCode)) {
         image = colorCode.split('').reduce((_img, code) => _img.replace(/\*/, code), image);
     }
 
