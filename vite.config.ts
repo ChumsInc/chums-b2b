0fe31ace5533bc33aa03af31c090929629b96ad4
Steve Montgomery <steve@chums.com>
2026-03-24 13:22:19 -0600

    fix: consolidate vite config with conditional build commands and ssr support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@ import 'dotenv/config';
 import {defineConfig} from 'vite';
 import process from "node:process";
 import {visualizer} from "rollup-plugin-visualizer";
-import {commonConfig} from "./vite.common.config.ts";
 
-export default defineConfig({
-    ...commonConfig,
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+
+const commonConfig = defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@/": path.resolve(process.cwd(), 'src'),
+            "@/api": path.resolve(process.cwd(), 'src/api'),
+            "@/app": path.resolve(process.cwd(), 'src/app'),
+            "@/components": path.resolve(process.cwd(), 'src/components'),
+            "@/constants": path.resolve(process.cwd(), 'src/constants'),
+            "@/ducks": path.resolve(process.cwd(), 'src/ducks'),
+            "@/hooks": path.resolve(process.cwd(), 'src/hooks'),
+            "@/slices": path.resolve(process.cwd(), 'src/slices'),
+            "@/types": path.resolve(process.cwd(), 'src/types'),
+            "@/utils": path.resolve(process.cwd(), 'src/utils'),
+        }
+    },
+});
+
+const serverConfig = defineConfig({
     server: {
         port: 8080,
         host: 'localhost',
@@ -27,26 +46,74 @@ export default defineConfig({
                 changeOrigin: true,
             }
         }
-    },
-    build: {
-        // minify: false,
-        outDir: 'dist-client',
-        emptyOutDir: true,
-        rolldownOptions: {
-            input: './src/client/index.tsx',
-            plugins: [visualizer({filename: 'stats.html', gzipSize: true})],
-            output: {
-                codeSplitting: {
-                    groups: [
-                        {name: 'react', test: /node_modules\/(react|react-dom)/},
-                        {name: 'mui', test: /node_modules\/@mui/},
-                        {name: 'vendor', test: /node_modules/},
-                    ]
+    }
+})
+
+export default defineConfig(({command, isSsrBuild}) => {
+    if (command === 'serve') {
+        return defineConfig({
+            ...commonConfig,
+            ...serverConfig
+        })
+    }
+    if (isSsrBuild) {
+        return defineConfig({
+            ...commonConfig,
+            build: {
+                outDir: 'dist-server',
+                lib: {
+                    entry: path.resolve(process.cwd(), 'src/server/index.ts'),
+                    name: 'chums-ssr',
+                    formats: ['es'],
+                },
+                emptyOutDir: true,
+                rolldownOptions: {
+                    input: path.resolve(process.cwd(), './src/server/index.ts'),
+                    output: {
+                        preserveModules: true,
+                        // codeSplitting: false
+                    },
+                    shimMissingExports: true,
+                },
+                ssr: true,
+                ssrManifest: true,
+                ssrEmitAssets: true,
+                manifest: true,
+                sourcemap: false,
+            },
+            ssr: {
+                resolve: {
+                    conditions: ['development', 'browser'],
+                    externalConditions: ['node']
+                },
+                optimizeDeps: {
+                    holdUntilCrawlEnd: true,
                 }
             }
-        },
-        manifest: true,
-        sourcemap: true,
-    },
+        })
+    }
 
+    return defineConfig({
+        ...commonConfig,
+        build: {
+            // minify: false,
+            outDir: 'dist-client',
+            emptyOutDir: true,
+            rolldownOptions: {
+                input: './src/client/index.tsx',
+                plugins: [visualizer({filename: 'stats.html', gzipSize: true})],
+                output: {
+                    codeSplitting: {
+                        groups: [
+                            {name: 'react', test: /node_modules\/(react|react-dom)/},
+                            {name: 'mui', test: /node_modules\/@mui/},
+                            {name: 'vendor', test: /node_modules/},
+                        ]
+                    }
+                }
+            },
+            manifest: true,
+            sourcemap: true,
+        },
+    })
 })
