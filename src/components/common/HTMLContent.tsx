c51b8f249b5684ef2e6323fd53bcc8216f8be0d7
Steve Montgomery <steve@chums.com>
2026-05-27 09:59:27 -0600

    feat: enhance HTMLContent to handle login-required sections and YouTube iframe cookie-free replacements dynamically

--- src/components/common/HTMLContent.tsx
+++ src/components/common/HTMLContent.tsx
@@ -1,36 +1,45 @@
-import {useEffect, useState} from 'react'
 import Box, {type BoxProps} from "@mui/material/Box";
 import {useAppSelector} from "@/app/hooks";
 import {selectAllowsMarketing} from "@/ducks/cookie-consent";
-
-interface FindReplace {
-    find: string|RegExp;
-    replace: string;
-}
-
-const replacements:FindReplace[] = [
-    {find: /youtube\.com/g, replace: 'youtube-nocookie.com'},
-];
+import parse from "html-react-parser";
+import {Element} from "domhandler";
+import ContentSectionRequiresLogin from "@/components/pages/ContentSectionRequiresLogin.tsx";
+import {selectLoggedIn} from "@/ducks/user/userProfileSlice.ts";
 
 export interface HTMLContentProps extends BoxProps {
     html: string;
 }
+
 export default function HTMLContent({html, ...rest}: HTMLContentProps) {
     const allowsMarketing = useAppSelector(selectAllowsMarketing);
-    const [htmlContent, setHtmlContent] = useState<string>(html);
-
-    useEffect(() => {
-        if (!allowsMarketing) {
-            const sanitized = replacements.reduce((_html, {find, replace}) => _html.replace(find, replace), html);
-            setHtmlContent(sanitized);
-            return;
-        }
-        setHtmlContent(html);
-    }, [html, allowsMarketing]);
+    const isLoggedIn = useAppSelector(selectLoggedIn);
 
     if (!html) {
         return null
     }
+    const parsed = parse(html, {
+        replace: (domNode) => {
+            if (domNode instanceof Element && domNode.attribs && domNode.attribs['data-login-required']
+                && !isLoggedIn) {
+                // Replace login-required sections with a login-required message
+                return (
+                    <ContentSectionRequiresLogin/>
+                )
+            }
+            if (!allowsMarketing) {
+                // Replace YouTube iframes with non-cookie-blocking iframes
+                if (domNode instanceof Element && domNode.name === 'iframe'
+                    && domNode.attribs && domNode.attribs.src && domNode.attribs.src.includes('youtube.com')) {
+                    return (
+                        <iframe {...domNode.attribs}
+                                src={domNode.attribs.src.replace('youtube.com', 'youtube-nocookie.com')}/>
+                    )
+                }
+            }
+            return domNode;
+        }
+    })
 
-    return <Box {...rest} dangerouslySetInnerHTML={{ __html: htmlContent }}  />
+
+    return (<Box {...rest}>{parsed}</Box>)
 }
